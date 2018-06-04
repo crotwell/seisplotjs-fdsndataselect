9914549870039fa00d5e9eb3e1b040f69e7b6183
Philip Crotwell <crotwell@seis.sc.edu>
2018-06-04 15:11:08 -0400

    const is better

--- test/calcStartEndDates.test.js
+++ test/calcStartEndDates.test.js
@@ -1,9 +1,9 @@
 import * as fdsndataselect from '../src/fdsndataselect';
 
 test("simple station", () => {
-  var duration = 300;
-  var clockOffset = 0;
-  var timeWindow = fdsndataselect.calcStartEndDates(null, null, duration, clockOffset);
+  const duration = 300;
+  const clockOffset = 0;
+  let timeWindow = fdsndataselect.calcStartEndDates(null, null, duration, clockOffset);
   expect(timeWindow.end.valueOf()).toEqual(timeWindow.start.valueOf()+duration*1000);
   expect(duration).toEqual(timeWindow.duration.asSeconds());
 });
